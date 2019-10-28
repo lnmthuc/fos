8e923af5f796f7003822a4eea1efc27c7604801c
hau nguyen <conghau.nguyen95@gmail.com>
2019-10-28 09:41:52 +0700

    fix provision

---
+++ fos-provision/FOS/FOS/Constant/Constant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Constant
+{
+    public static class Constant
+    {
+        public const string siteUrl = "https://devpreciovn.sharepoint.com/sites/FOS/";
+        public const string eventContentTypeName = "Event";
+        public const string eventListName = "Event Sample";
+        public const string calendarContentTypeName = "EventCalendar";
+        public const string calendarContentTypeGroupName = "EventCalendarGroup";
+        public const string contentTypeName = "contenttypeName";
+        public const string listName = "list";
+        public const string calendarViewName = "EventCalendarView";
+        public const string testabc = "";
+    }
+}
